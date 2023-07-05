4b8d829486a0669a2c51643ac5c8777b907c890c
gopib4599 <86304860+gopib4599@users.noreply.github.com>
2023-07-05 10:21:22 +0530

    Update tomcatDev.groovy

--- vars/tomcatDev.groovy
+++ vars/tomcatDev.groovy
@@ -2,7 +2,7 @@ def call (creds,ip,warfile){
    //  install ssh agent plugin
    sshagent(['creds']) {
                // copy war file to tomcat-dev server
-                sh "scp -o StrictHostKeyChecking=no target/${warfile} ubuntu@ip:/opt/tomcat9/webapps"
+                sh "scp -o StrictHostKeyChecking=no target/${warfile} ubuntu@${ip}:/opt/tomcat9/webapps"
                 
                 sh "ssh ubuntu@${ip} /opt/tomcat9/bin/shutdown.sh"
                 sh "ssh ubuntu@${ip} /opt/tomcat9/bin/startup.sh"
